cda77ccc7703cba6fa79d26499c6eaa00ba1324b
delarsen <92631995+delarsen@users.noreply.github.com>
2025-04-18 19:52:47 +0300

    Frontend

--- main.py
+++ main.py
@@ -7,4 +7,19 @@ app = Flask(__name__)
 def main_page():
     return render_template("main_page.html")
 
+@app.route("/menu/")
+def menu():
+    return render_template("menu_page.html")
+
+@app.route("/change/")
+def change():
+    return render_template("change_page.html")
+
+@app.route("/order/")
+def order():
+    return render_template("order_page.html")
+
 app.run(debug=True)
+
+
+#<button onclick="addToCart('Паста Карбонара')">Добавить в заказ</button>
