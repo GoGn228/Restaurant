3d44907bcfc0fad74364bdabfcdfb720c3643da1
delarsen <92631995+delarsen@users.noreply.github.com>
2025-05-02 19:45:35 +0300

    02.05

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+from sys import orig_argv
+from flask import Flask, render_template, request, session
 import json
 import sqlite3
+
 app = Flask(__name__)
+app.secret_key = "super_secret_key_123"
 
 con = sqlite3.connect("food.db", check_same_thread=False)
 cursor = con.cursor()
@@ -17,6 +20,13 @@ def menu():
 @app.route("/change/")
 def change():
     dish = request.args.get("dish")
+    def get_price():
+        conn = sqlite3.connect('food.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT price FROM food_menu WHERE name = ?", (dish,))
+        price = cursor.fetchone()
+        conn.close()
+        return price[0] if price else 0  # Гарантируем, что вернётся число
     cursor.execute('SELECT description FROM food_menu WHERE name=(?)', (dish,))
     description = cursor.fetchall()
     cursor.execute('SELECT img FROM food_menu WHERE name=(?)', (dish,))
@@ -33,12 +43,23 @@ def change():
     add5 = cursor.fetchall()
     cursor.execute('SELECT add6 FROM food_menu WHERE name=(?)', (dish,))
     add6 = cursor.fetchall()
-    return render_template("change_page.html", dish=dish, description=description, img=img, add1=add1, add2=add2, add3=add3, add4=add4, add5=add5, add6=add6)
+    cursor.execute('SELECT price FROM food_menu WHERE name=(?)', (dish,))
+    price = get_price()
+    selected_ingredients = request.args.getlist("ingredientss")
+    if "price" not in session:
+        session["price"] = get_price() # Сохраняем цену в session
+    print(selected_ingredients, price)
+    base_price = float(price[0][0]) if price and price[0][0] else 0  # Проверяем, есть ли число
+    selected_ingredients = [float(ing) for ing in selected_ingredients if ing.replace('.', '', 1).isdigit()]
+    total_price = price + sum(selected_ingredients)
+    print("Итоговая цена:", total_price)  # Проверяем финальное значение
+    quantity = request.args.get("quantity", type=int, default=1)
+    return render_template("change_page.html", dish=dish, description=description, img=img, add1=add1, add2=add2, add3=add3, add4=add4, add5=add5, add6=add6, quantity=quantity, total_price=total_price, price=session["price"])
 
 @app.route("/order/")
 def order():
-    quantity = request.args.get("quantity", type=int, default=1)
     return render_template("order_page.html")
+
 
 app.run(debug=True)
 
